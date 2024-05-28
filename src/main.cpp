269914a1ac2232e3119fbaaab36074244b6a0c5f
square <martinmariollanos@gmail.com>
2024-05-28 03:11:32 -0400

    Documentacion de funcion parse_line()

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,15 @@
 #include <vector>
 #include <string>
 #include <math.h>
+#include <open_hash.h>
+#include <hash_functions.h>
 
+#define CSV_NUM_COLUMNS 7
+#define CSV_NUM_ENTRIES 21070
+
+// Modifica una string de notación científica a forma decimal (no cambia tipo de dato)
+// nota: esta funcion destruye la string de entrada
 std::string SciNot_to_ull(std::string& scinot) {
-    // nota: esta funcion destruye la string de entrada
     int plus_idx = scinot.find('+');
     int scipow = std::stoi(scinot.substr(plus_idx,scinot.size()-plus_idx));
     scinot.erase(plus_idx-1,scinot.size()-1);
@@ -16,18 +22,18 @@ std::string SciNot_to_ull(std::string& scinot) {
     return scinot;
 }
 
-void parse_line(std::ifstream& file, std::vector<uniflw_values*>& container, size_t n_parses) {
-    std::string line;
-    std::getline(file,line); // skip first line
-    size_t line_count = 0;
+void parse_line(std::ifstream& file, std::vector<twtdata*>& TEMP_container, size_t n_parses) {
+    std::string line; // almacena la fila que se lee en cada iteración
+    std::getline(file,line); // para saltarse la primera fila
+    size_t line_count = 0; // numero de filas leidas
 
     while (line_count < n_parses) {
         std::getline(file,line);
-        uniflw_values* line_values = new uniflw_values;
+        twtdata* row_values = new twtdata;
         int idx = 0;
         int line_sz = line.size();
 
-        for (int col = 0; col < 7; col++) {
+        for (int col = 0; col < CSV_NUM_COLUMNS; col++) {
             std::string substring = line.substr(idx,line_sz-idx);
             int end_idx;
             if (substring.find(',') == std::string::npos) {
@@ -36,28 +42,28 @@ void parse_line(std::ifstream& file, std::vector<uniflw_values*>& container, siz
             else {
                 end_idx = idx+substring.find(',');
             }
-            /* std::cout << "col: " << col << " start_idx: " << idx << " end_idx: " << end_idx << std::endl;
-            *   debugging print
-            */
-            std::string value_member = line.substr(idx,end_idx-idx);
-            if (value_member.find("E+") != std::string::npos) {
-                value_member = SciNot_to_ull(value_member);
+            /* debugging print */
+            // std::cout << "col: " << col << " start_idx: " << idx << " end_idx: " << end_idx << std::endl;
+            std::string line_member = line.substr(idx,end_idx-idx);
+            if (line_member.find("E+") != std::string::npos) { // revisar si el sub-string leido en la fila contiene notacion cientifica. Esto podría moverse fuera del for loop.
+                line_member = SciNot_to_ull(line_member);
             }
-            line_values->init_from_str(col, value_member);
+            row_values->init_from_str(col, line_member);
             idx = end_idx+1;
         }
-        container.push_back(line_values);
+        TEMP_container.push_back(row_values);
         line_count++;
     }
 }
 
 int main(int argc, char *argv[])
 {
-    std::vector<uniflw_values*> userdata;
+    std::vector<twtdata*> userdata;
     std::ifstream stream;
     stream.open("universities_followers.csv", std::ifstream::in);
-    parse_line(stream, userdata, 4);
+    parse_line(stream, userdata, 3);
     for (auto& v : userdata) v->print_data();
+    std::cout << hc_compsum(44) << std::endl;
 
     stream.close();
     return 0;
