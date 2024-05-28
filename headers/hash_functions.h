9fdc65094d341e10daf9a67d94eba3297b341e1c
square <martinmariollanos@gmail.com>
2024-05-28 03:10:58 -0400

    Algunas hash code/compressors para claves numericas

---
+++ headers/hash_functions.h
@@ -0,0 +1,28 @@
+#include <math.h>
+#include <string>
+
+#ifndef CUSTOM_HASH_FUNCTIONS_H
+#define CUSTOM_HASH_FUNCTIONS_H
+
+/* Funciones de compresión con clave UNSIGNED LONG LONG INT */
+
+// Metodo de la division
+int h_div(unsigned long long int key, int size) {
+    return key % size;
+}
+
+// Metodo multiply, add and divide
+int h_MAD(unsigned long long int key, int mult, int add, int size) {
+    return (int)((mult*key + add) % size);
+}
+
+/* Hash Codes con clave ULL */
+
+// Component Sum (dos particiones de 32 bits. Implementacion cortesía del PPT 8: Diccionarios)
+int hc_compsum(unsigned long long int i) {
+    return ((int) i + (int) (i>>32));
+}
+
+/* Funciones con clave STRING */
+
+#endif
