89add0098dec9e26df5ca8d5aec7aae2f7a61437
square <martinmariollanos@gmail.com>
2024-05-28 22:19:42 -0400

    Clase abstracta MapADT ahora es de Clave y Dato genericos

--- headers/map_ADT.h
+++ headers/map_ADT.h
@@ -4,32 +4,13 @@
 #ifndef CUSTOM_MAP_ADT_H
 #define CUSTOM_MAP_ADT_H
 
-template <typename dataT>
-class Map_ULL {
-    virtual dataT get(unsigned long long int key);
-    virtual dataT put(unsigned long long int key, dataT data);
-    virtual dataT remove(unsigned long long int key);
+template <typename KeyType, typename DataType>
+class MapADT {
+    virtual DataType get(KeyType key);
+    virtual DataType put(KeyType key, DataType data);
+    virtual DataType remove(KeyType key);
     virtual int size();
     virtual bool isEmpty();
-};
-
-template <typename dataT>
-class Map_S {
-    virtual dataT get(std::string key);
-    virtual dataT put(std::string key, dataT data);
-    virtual dataT remove(std::string key);
-    virtual int size();
-    virtual bool isEmpty();
-};
-
-struct Pair_ULL {
-    unsigned long long int key;
-    twtdata value;
-};
-
-struct Pair_S {
-    std::string key;
-    twtdata value;
 };
 
 #endif
