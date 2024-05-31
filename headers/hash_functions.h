faa9cd3c88a46c4cebe9d94cb38be05d2d436549
square <martinmariollanos@gmail.com>
2024-05-31 19:52:41 -0400

    Cambio de parametros en funcion hash de strings + documentacion

--- headers/hash_functions.h
+++ headers/hash_functions.h
@@ -7,7 +7,18 @@
 
 const unsigned int _POLY_Z = 33; // Constante Z para polinomio de acumulacion (string hashing)
 
-/* Funciones de compresión con clave UNSIGNED LONG LONG INT */
+// Declaracion de funciones
+
+/* Funciones de compresión con clave numérica */
+int h_div(unsigned long long int key, int size);
+int h_MAD(unsigned long long int key, int mult, int add, int size);
+/* Hash Codes con clave numérica */
+int hc_compsum(unsigned long long int i);
+/* Hash Codes con clave string */
+unsigned long long int hc_poly_accumulation(std::string key);
+/* Funciones hash listas para usar en tablas */
+int CSandCompress(unsigned long long int key, int size);
+int string_hash(std::string key, int size);
 
 // Metodo de la division
 int h_div(unsigned long long int key, int size) 
@@ -25,15 +36,12 @@ int h_MAD(unsigned long long int key, int mult, int add, int size)
     return (int)((mult*key + add) % size);
 }
 
-/* Hash Codes con clave ULL */
-
 // Component Sum (dos particiones de 32 bits. Implementacion cortesía del PPT 8: Diccionarios)
 int hc_compsum(unsigned long long int i) 
 {
     return ((int) i + (int) (i>>32));
 }
 
-// Funciones "completas"
 int CSandCompress(unsigned long long int key, int size) 
 {
     return h_div((unsigned long long int)hc_compsum(key),size);
@@ -47,25 +55,25 @@ int bogohash(unsigned long long int key, int size)
 }
 */
 
-/* Hash Codes con clave String*/
-
 // Acumulacion polinomial
-unsigned long long int poly_accumulation(std::string key) 
+unsigned long long int hc_poly_accumulation(std::string key) 
 {
+    // Este valor es muy importante, reducirlo aumenta la velocidad de insercion, pero podria aumentar el % de colisiones?
+    const int MAX_CHARS_TO_ADD = 7;
+
     unsigned int poly_eval = 0;
     int z_exp = 0;
-    for (auto& c : key) {
-        poly_eval += ((int)c)*pow(_POLY_Z,z_exp);
+    int strsize = key.size();
+
+    for (int i = 0; i < strsize && i < MAX_CHARS_TO_ADD; i++) {
+        poly_eval += ((int)key.at(i))*pow(_POLY_Z,z_exp);
         z_exp++;
     }
     return poly_eval;
 }
 
-// Funciones "completas"
-
 int string_hash(std::string key, int size) {
-    return h_div(poly_accumulation(key),size);
+    return h_div(hc_poly_accumulation(key),size);
 }
-
 
 #endif
