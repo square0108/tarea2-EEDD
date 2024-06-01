38ad9e5de39153323800d6c744c57fd0b093312a
square <martinmariollanos@gmail.com>
2024-06-01 18:43:56 -0400

    Testing de inserciones listo para todo tipo de hash table

--- src/insert_entries.cpp
+++ src/insert_entries.cpp
@@ -12,10 +12,21 @@
 #include "csv_data_insertion.h"
 #include "closed_hash.h"
 
-#define BIG_PRIME_SIZE 22189
+// Se inicializan todas las tablas hash con esta constante. Algunos otros primos grandes: {12821,17977,33533,65537,139969}.
+#define BIG_PRIME_SIZE 33533
 const bool _main_debug = 1;
+double total_running_time = 0;
 
 using namespace std;
+
+/* Clase que almacena unicamente funciones estaticas para realizar experimentos sobre el dataset de Twitter.
+*/
+template <typename KeyT>
+class TableTester {
+    public:
+        static void insertion_test(std::vector<userdata>& dataset, MapADT<KeyT,userdata>& hash_table, int n_insertions);
+        static void insertion_test(std::vector<userdata>& dataset, std::unordered_map<KeyT,userdata>& map, int n_insertions);
+};
 
 /* IMPORTANTE: El codigo no puede ejecutarse solo con ./main, deben proporcionarse 3 argumentos en este orden:
 *
@@ -34,7 +45,6 @@ int main(int argc, char **argv)
         cout << "Invalid syntax. Usage: ./main <table type> <key type> <number of insertions>" << endl;
 
     ifstream twitter_csv;
-    double total_running_time = 0;
     char* table_type = argv[1];
     char* key_type = argv[2];
     size_t num_elements = atoi(argv[3]);
@@ -47,96 +57,63 @@ int main(int argc, char **argv)
     
     // Inserciones para claves tipo USERNAME
     if (strcmp(key_type,"username") == 0) {
-
         // Clave USERNAME ; Tabla STL MAP
         if (strcmp(table_type,"STL_map") == 0) {
-            
             std::unordered_map<std::string,userdata> map;
-            userdata current_row;
-
-            for (userdata& row : twitter_values) {
-                current_row = row;
-                auto start = chrono::high_resolution_clock::now();
-                map.insert({current_row.username,current_row});
-                auto end = chrono::high_resolution_clock::now();
-                total_running_time += (1e-9)*(chrono::duration_cast<chrono::nanoseconds>(end-start).count());
-            }
-
+            TableTester<string>::insertion_test(twitter_values,map,num_elements);
         }
 
         // Clave USERNAME ; Tabla OPEN HASH
         else if (strcmp(table_type,"open_hash") == 0) {
-
             tarea::OpenHashTable<string,userdata> openhash(BIG_PRIME_SIZE,string_hash);
-            userdata current_row;
-
-            for (userdata& row : twitter_values) {
-                current_row = row;
-                auto start = chrono::high_resolution_clock::now();
-                openhash.put(current_row.username,current_row);
-                auto end = chrono::high_resolution_clock::now();
-                total_running_time += (1e-9)*(chrono::duration_cast<chrono::nanoseconds>(end-start).count());
-            }
-
-            openhash.get("cristobalquirog").print_data();
-
-            openhash.get_collisions_vector();
+            TableTester<string>::insertion_test(twitter_values,openhash,num_elements);
         }
 
-        // Clave USERNAME ; Tabla CLOSED HASH
-        else if (strcmp(table_type,"closed_hash") == 0) {
-            ClosedHashTable<string,userdata> closedhash(BIG_PRIME_SIZE,string_hash,LINEAR_PROBING);
-            userdata current_row;
+        // Clave USERNAME ; Tablas CLOSED HASH
+        else if (strcmp(table_type,"closed_hash_linear") == 0) {
+            ClosedHashTable<string, userdata> closed_hash(BIG_PRIME_SIZE,string_hash,LINEAR_PROBING);
+            TableTester<string>::insertion_test(twitter_values,closed_hash, num_elements);
+        }
 
-            for (userdata& row : twitter_values) {
-                current_row = row;
-                auto start = chrono::high_resolution_clock::now();
-                closedhash.put(current_row.username,current_row);
-                auto end = chrono::high_resolution_clock::now();
-                total_running_time += (1e-9)*(chrono::duration_cast<chrono::nanoseconds>(end-start).count());
-            }
+        else if (strcmp(table_type,"closed_hash_quadratic") == 0) {
+            ClosedHashTable<string, userdata> closed_hash(BIG_PRIME_SIZE,string_hash,QUADRATIC_PROBING);
+            TableTester<string>::insertion_test(twitter_values,closed_hash, num_elements);
+        }
 
-            closedhash.get("aedearica").print_data();
-            
+        else if (strcmp(table_type,"closed_hash_quadratic") == 0) {
+            ClosedHashTable<string, userdata> closed_hash(BIG_PRIME_SIZE,string_hash,DOUBLE_HASHING);
+            TableTester<string>::insertion_test(twitter_values,closed_hash, num_elements);
         }
     }
 
     // Inserciones para claves tipo USER ID
     else if (strcmp(key_type,"user_id") == 0) {
-        
         // Clave ID ; Tabla STL MAP
         if (strcmp(table_type,"STL_map") == 0) {
-                        
             std::unordered_map<unsigned long long int,userdata> map;
-            userdata current_row;
-
-            for (userdata& row : twitter_values) {
-                current_row = row;
-                auto start = chrono::high_resolution_clock::now();
-                map.insert({current_row.user_id,current_row});
-                auto end = chrono::high_resolution_clock::now();
-                total_running_time += (1e-9)*(chrono::duration_cast<chrono::nanoseconds>(end-start).count());
-            }
-
+            TableTester<unsigned long long int>::insertion_test(twitter_values,map,num_elements);
         }
 
         // Clave ID ; Tabla OPEN HASH
         else if (strcmp(table_type,"open_hash") == 0) {
             tarea::OpenHashTable<unsigned long long int,userdata> openhash(BIG_PRIME_SIZE,CSandCompress);
-            userdata current_row;
-            for (auto& row : twitter_values) {
-                current_row = row;
-                auto start = chrono::high_resolution_clock::now();
-                openhash.put(current_row.user_id,current_row);
-                auto end = chrono::high_resolution_clock::now();
-
-                total_running_time += (1e-9)*(chrono::duration_cast<chrono::nanoseconds>(end-start).count());
-            }
+            TableTester<unsigned long long int>::insertion_test(twitter_values,openhash,num_elements);
         }
 
         // Clave ID ; Tabla CLOSED HASH
-        else if (strcmp(table_type,"closed_hash") == 0) {
-            // pendiente
+        else if (strcmp(table_type,"closed_hash_linear") == 0) {
+            ClosedHashTable<unsigned long long int, userdata> closed_hash(BIG_PRIME_SIZE,CSandCompress,LINEAR_PROBING);
+            TableTester<unsigned long long int>::insertion_test(twitter_values,closed_hash, num_elements);
+        }
+
+        else if (strcmp(table_type,"closed_hash_quadratic") == 0) {
+            ClosedHashTable<unsigned long long int, userdata> closed_hash(BIG_PRIME_SIZE,CSandCompress,QUADRATIC_PROBING);
+            TableTester<unsigned long long int>::insertion_test(twitter_values,closed_hash, num_elements);
+        }
+
+        else if (strcmp(table_type,"closed_hash_quadratic") == 0) {
+            ClosedHashTable<unsigned long long int, userdata> closed_hash(BIG_PRIME_SIZE,CSandCompress,DOUBLE_HASHING);
+            TableTester<unsigned long long int>::insertion_test(twitter_values,closed_hash, num_elements);
         }
     }
 
@@ -150,4 +127,48 @@ int main(int argc, char **argv)
     cout << (string)key_type << ";" << num_elements << ";" << total_running_time << endl;
     twitter_csv.close();
     return 0;
+}
+
+template <typename KeyT>
+void TableTester<KeyT>::insertion_test(std::vector<userdata>& dataset,MapADT<KeyT,userdata>& hash_table,int n_insertions) {
+    if constexpr (std::is_same_v<KeyT, std::string>) {
+        for (int i = 0; i < n_insertions; i++) {
+            userdata current_user = dataset.at(i);
+            auto start = chrono::high_resolution_clock::now();
+            hash_table.put(current_user.username,current_user);
+            auto end = chrono::high_resolution_clock::now();
+            total_running_time += (1e-9)*(chrono::duration_cast<chrono::nanoseconds>(end-start).count());
+        }
+    }
+    else if constexpr (std::is_same_v<KeyT, unsigned long long int>) {
+        for (int i = 0; i < n_insertions; i++) {
+            userdata current_user = dataset.at(i);
+            auto start = chrono::high_resolution_clock::now();
+            hash_table.put(current_user.user_id,current_user);
+            auto end = chrono::high_resolution_clock::now();
+            total_running_time += (1e-9)*(chrono::duration_cast<chrono::nanoseconds>(end-start).count());
+        }
+    }
+}
+
+template <typename KeyT>
+void TableTester<KeyT>::insertion_test(std::vector<userdata>& dataset, std::unordered_map<KeyT,userdata>& map, int n_insertions) {
+    if constexpr (std::is_same_v<KeyT, std::string>) {
+        for (int i = 0; i < n_insertions; i++) {
+            userdata current_user = dataset.at(i);
+            auto start = chrono::high_resolution_clock::now();
+            map.insert({current_user.username,current_user});
+            auto end = chrono::high_resolution_clock::now();
+            total_running_time += (1e-9)*(chrono::duration_cast<chrono::nanoseconds>(end-start).count());
+        }
+    }
+    else if constexpr (std::is_same_v<KeyT, unsigned long long int>) {
+        for (int i = 0; i < n_insertions; i++) {
+            userdata current_user = dataset.at(i);
+            auto start = chrono::high_resolution_clock::now();
+            map.insert({current_user.user_id,current_user});
+            auto end = chrono::high_resolution_clock::now();
+            total_running_time += (1e-9)*(chrono::duration_cast<chrono::nanoseconds>(end-start).count());
+        }
+    }
 }
